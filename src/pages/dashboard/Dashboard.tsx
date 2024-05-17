4ea38027d30c418fa02efa55794982c767de3f65
Ajala Matthew Adekunle <80929400+SymplyMatt@users.noreply.github.com>
2024-05-17 15:17:24 +0100

    projects navigation

--- src/pages/dashboard/Dashboard.tsx
+++ src/pages/dashboard/Dashboard.tsx
@@ -1,37 +1,29 @@
-    import { ReactNode, useEffect } from "react";
-    import Sidebar from "../../components/dashboard/Sidebar"
-    import TopMain from "../../components/dashboard/TopMain"
-import { useLocation, useNavigate } from "react-router-dom";
-    interface DashboardLayoutProps {
-        children?: [ReactNode, ReactNode, ReactNode?];
-        background?: String;
-    }
-    const Dashboard: React.FC<DashboardLayoutProps> = ({children = [<></>, <></>, <></>], background='dashboardColor'}) => {
-        const [header, content, overlay] = children;
-        const location = useLocation();
-        const navigate = useNavigate();
-        useEffect(()=>{
-            if(!location.state?.sessionHash || !location.state?.email ){
-              navigate('/', {replace : true, state: null});
-            }
-        },[]);
-    return (
-        <>
-            {location.state?.auth && <div className="grid grid-cols-10 w-screen h-screen relative">
-                <Sidebar />
-                <div className={`h-screen bg-${background} col-span-8 relative overflow-hidden`}>
-                    <div className="flex flex-col justify-between">
-                        <TopMain />
-                        {header}
-                    </div>
-                    {content}
+import { ReactNode } from "react";
+import Sidebar from "../../components/dashboard/Sidebar"
+import TopMain from "../../components/dashboard/TopMain"
+interface DashboardLayoutProps {
+    children?: [ReactNode, ReactNode, ReactNode?];
+    background?: String;
+}
+const Dashboard: React.FC<DashboardLayoutProps> = ({children = [<></>, <></>, <></>], background='dashboardColor'}) => {
+    const [header, content, overlay] = children;
+return (
+    <>
+        {<div className="grid grid-cols-10 w-screen h-screen relative">
+            <Sidebar />
+            <div className={`h-screen bg-${background} col-span-8 relative overflow-hidden`}>
+                <div className="flex flex-col justify-between">
+                    <TopMain />
+                    {header}
                 </div>
-                { overlay && <div className={`absolute h-screen w-screen bg-blackLight flex justify-center items-center`}>
-                    {overlay}
-                </div>}
+                {content}
+            </div>
+            { overlay && <div className={`absolute h-screen w-screen bg-blackLight flex justify-center items-center`}>
+                {overlay}
             </div>}
-        </>
-    )
-    }
+        </div>}
+    </>
+)
+}
 
-    export default Dashboard
+export default Dashboard
