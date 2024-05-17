2d0d47ed7aba631479b879975b59e1c4de0cef0b
Ajala Matthew Adekunle <80929400+SymplyMatt@users.noreply.github.com>
2024-05-17 14:28:43 +0100

    HOME PAGE RESPONSIVE

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -33,7 +33,7 @@ const Home = () => {
             </div>
         </div>
         <div className="flex flex-col justify-center items-center gap-20">
-            <div className="text-recruitBlue font-extrabold text-[40px] sm:text-[60px] text-center sm:big-text font-hiragino-sans">The simplest way to manage project teams</div>
+            <div className="text-recruitBlue font-extrabold text-[40px] sm:text-[60px] text-center big-text font-hiragino-sans">The simplest way to manage project teams</div>
             <div className="text-recruitBlue max-w-[700px] flex justify-center items-center m-auto text-center">A tool for project managers to easily track project lifespan and team members engagement during the course of the project </div>
             <div className="flex flex-col justify-center gap-10">
               <div className="flex justify-center p-10 border-solid border border-gray-borderGray rounded-xl gap-100">
