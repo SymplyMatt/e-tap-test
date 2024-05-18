0399c822126806ec803411111b64e93a2669dcb7
Ajala Matthew Adekunle <80929400+SymplyMatt@users.noreply.github.com>
2024-05-18 16:52:54 +0100

    base64 image

--- src/components/dashboard/projects/create/CoverPhoto.tsx
+++ src/components/dashboard/projects/create/CoverPhoto.tsx
@@ -1,32 +1,62 @@
-import registration_details from '../../../../assets/images/registration_details.svg'
-import select_photo from '../../../../assets/images/select_photo.svg'
+import { useState } from 'react';
+import registration_details from '../../../../assets/images/registration_details.svg';
+import select_photo from '../../../../assets/images/select_photo.svg';
+import utils from '../../../../utils/utils';
+
 interface ComponentProps {
-    step: number
-    setStep: Function
-}
-const CoverPhoto : React.FC<ComponentProps> =  ({step,setStep}) => {
-  return (
-        <div className="my-10 py-10 create-project flex flex-col w-full gap-10">
-            <div className="flex flex-col items-center gap-50 mb-30">
-                <div className="flex flex-col items-center">
-                    <div className="font-hiragino text-[32px] font-bold">Registration details</div>
-                    <div className="font-normal">Set the required details you want from each applicant </div>
-                </div>
-                <div className="flex items-center justify-center  flex-col gap-5">
-                    <img src={registration_details} alt="" className='h-[200px]'/>
-                    <div className="rounded-8 font-normal text-14 border border-borderGray py-5 px-10 flex gap-5 items-center justify-center cursor-pointer relative"><img src={select_photo} alt="" className='cursor-pointer'/>
-                        Select cover image
-                        <input type="file" className='absolute h-full w-full top-0 opacity-0 cursor-pointer' />
-                    </div>
-                </div>
-            </div>
-            <div className="px-20 flex w-full py-10  justify-between items-center border border-borderGray shadow">
-                <div className="font-semibold text-recruitBlue cursor-pointer flex items-center gap-10"onClick={()=> setStep(step - 1)}><i className="fa-solid fa-arrow-left"></i>Go Back</div>
-                <button className={`py-10 px-30 bg-recruitBlue text-white h-[45px] flex items-center justify-center cursor-pointer rounded-16 flex gap-10`} onClick={()=> setStep(step + 1)}>Continue <i className="fa-solid fa-arrow-right"></i></button>
-            </div>
-            <div className="h-[50px] w-full bg-inherit"></div>
-        </div>
-  )
+  step: number;
+  setStep: Function;
 }
 
-export default CoverPhoto
+const CoverPhoto: React.FC<ComponentProps> = ({ step, setStep }) => {
+  const [image, setImage] = useState<any>('');
+  const [image64, setImage64] = useState<string>('');
+
+  const handleImageChange = async (event: React.ChangeEvent<HTMLInputElement>) => {
+    const file = event.target.files?.[0];
+    if (file) {
+      try {
+        const base64String = await utils.convertImageToBase64(file);
+        setImage(URL.createObjectURL(file));
+        
+        setImage64(`data:image/jpeg;base64,${base64String}`);
+      } catch (error) {
+        console.error("Error converting image to base64:", error);
+      }
+    }
+  };
+
+  return (
+    <div className="my-10 py-10 create-project flex flex-col w-full gap-10">
+      <div className="flex flex-col items-center gap-50 mb-30">
+        <div className="flex flex-col items-center">
+          <div className="font-hiragino text-[32px] font-bold">Registration details</div>
+          <div className="font-normal">Set the required details you want from each applicant</div>
+        </div>
+        <div className="flex items-center justify-center flex-col gap-5">
+          <img src={image64 ? image64 : registration_details} alt="" className='h-[200px] rounded-8' />
+          <div className="rounded-8 font-normal text-14 border border-borderGray py-5 px-10 flex gap-5 items-center justify-center cursor-pointer relative">
+            <img src={select_photo} alt="" className='cursor-pointer' />
+            Select cover image
+            <input
+              type="file"
+              className='absolute h-full w-full top-0 opacity-0 cursor-pointer'
+              onChange={handleImageChange}
+            />
+          </div>
+        </div>
+      </div>
+      <div className="px-20 flex w-full py-10 justify-between items-center border border-borderGray shadow">
+        <div className="font-semibold text-recruitBlue cursor-pointer flex items-center gap-10" onClick={() => setStep(step - 1)}>
+          <i className="fa-solid fa-arrow-left"></i>Go Back
+        </div>
+        <button className={`py-10 px-30 bg-recruitBlue text-white h-[45px] flex items-center justify-center cursor-pointer rounded-16 flex gap-10`} onClick={() => setStep(step + 1)}>
+          Continue <i className="fa-solid fa-arrow-right"></i>
+        </button>
+      </div>
+      <div className="h-[50px] w-full bg-inherit"></div>
+    </div>
+  );
+}
+
+export default CoverPhoto;
--- src/utils/utils.tsx
+++ src/utils/utils.tsx
@@ -15,4 +15,21 @@ export default class utils {
         const phoneRegex: RegExp = /^\d{10}$/;
         return phoneRegex.test(number);
     }
+    static convertImageToBase64(file: File): Promise<string> {
+      
+      return new Promise((resolve, reject) => {
+        const reader = new FileReader();
+        reader.onloadend = () => {
+          if (reader.result) {
+            resolve(reader.result.toString().split(',')[1]);
+          } else {
+            reject(new Error("Failed to convert file to base64."));
+          }
+        };
+        reader.onerror = (error) => {
+          reject(error);
+        };
+        reader.readAsDataURL(file);
+      });
+    }
 }
