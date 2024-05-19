115f219e4c966b0474439d398a479e410429af54
Ajala Matthew Adekunle <80929400+SymplyMatt@users.noreply.github.com>
2024-05-19 01:09:20 +0100

    user info state

--- src/context/DashboardContext.tsx
+++ src/context/DashboardContext.tsx
@@ -1,23 +1,40 @@
 import { createContext, useState, PropsWithChildren} from 'react';
 
+interface userInfo{
+  organizationId: string;
+  emailaddress: string;
+  fullName : string;
+  image_url : string;
+  role : string;
+  phone : string;
+  can : string;
+  exp : string;
+}
 
 interface ContextValue {
     token: string | null;
     setToken : Function;
+    user: any | null;
+    setUser : Function;
 }
 
 export const Context = createContext<ContextValue>({
     token : null,
-    setToken : ()=>{}
+    setToken : ()=>{},
+    user : null,
+    setUser : ()=>{}
 });
 
 const DashboardContext = ({ children }: PropsWithChildren<{}>) => {
     const [token, setToken] = useState<string | null>(null);
+    const [user, setUser] = useState<userInfo | null>(null);
 
     
     const value: ContextValue = {
         token,
-        setToken
+        setToken,
+        user,
+        setUser
     };
 
   return (
--- src/pages/dashboard/DashboardMiddleware.tsx
+++ src/pages/dashboard/DashboardMiddleware.tsx
@@ -1,18 +1,32 @@
 import {  Outlet, useLocation, useNavigate } from 'react-router-dom';
 import { useContext, useEffect, useState } from 'react';
 import { Context } from '../../context/DashboardContext';
+import utils from '../../utils/utils';
 
 const DashboardMiddleware: React.FC = () => {
-  const { token , setToken} = useContext(Context);
+  const { token , setToken, setUser, user} = useContext(Context);
   const [isAuthenticated, setIsAuthenticated] = useState(false);
   const location = useLocation();
   const navigate = useNavigate();
   useEffect(()=>{
-    if(!token){
+    if(!token || !user){
         if (location.state?.userInfo || localStorage.getItem('userInfo')) {
             const userInfo = localStorage.getItem('userInfo');
             const token = location.state?.userInfo.accessToken ? location.state?.userInfo.accessToken : userInfo ? JSON.parse(userInfo)?.accessToken : null;
+            
             if(token){
+                const decodedToken = utils.decodeJWT(token);
+                setUser({
+                    organizationId: decodedToken["http://schemas.xmlsoap.org/ws/2005/05/identity/claims/nameidentifier"],
+                    emailaddress: decodedToken["http://schemas.xmlsoap.org/ws/2005/05/identity/claims/emailaddress"],
+                    fullName : decodedToken["fullName"],
+                    image_url : decodedToken["image_url"],
+                    role : decodedToken["role"],
+                    phone : decodedToken["http://schemas.xmlsoap.org/ws/2005/05/identity/claims/mobilephone"],
+                    can : decodedToken['can'],
+                    exp : decodedToken["exp"]
+                  });
+                console.log('decoded token: ', decodedToken);
                 setToken(token);
                 setIsAuthenticated(true);
             }else{
