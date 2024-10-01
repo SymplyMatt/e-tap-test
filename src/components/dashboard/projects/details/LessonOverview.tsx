be998b9aba635aace5eed027a50ec1e3faf9e383
Ajala Matthew Adekunle <80929400+SymplyMatt@users.noreply.github.com>
2024-10-01 22:57:13 +0100

    lesson progress

--- src/components/dashboard/projects/details/LessonOverview.tsx
+++ src/components/dashboard/projects/details/LessonOverview.tsx
@@ -1,6 +1,8 @@
-import { useEffect, useRef } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import { Topic } from '../../../../utils/interfaces';
 import { useLocation } from 'react-router-dom';
+import makeRequest from '../../../../services/axios';
+import utils from '../../../../utils/utils';
 
 interface Props {
   topic: Topic;
@@ -9,12 +11,40 @@ interface Props {
 function LessonOverview({ topic }: Props) {
   const videoRef = useRef<HTMLVideoElement | null>(null);
   const location = useLocation();
+  const [lesson, setLesson] = useState<any>(null);
+  const [hasLoadedProgress, setHasLoadedProgress] = useState(false);
+  useEffect(()=>{
+    console.log(location.state?.lesson);
+    if(location.state?.lesson){
+      setLesson(location.state.lesson);
+    }else{
+      async function createLesson(){
+        console.log(`attempting to create lesson`);
+        const res: any = await makeRequest('POST', `/lessons/start`,null,{topicId: topic.id});
+        console.log(`create lesson: `, res);
+        if(res.status === 200){
+          const newLesson = res.data;
+          setLesson(newLesson);
+        }else{
+          utils.createErrorNotification('Unable to start lesson', 1000);
+        }
+      }
+      createLesson()
+    }
+    setHasLoadedProgress(true);
+  },[]);
+  async function updateProgress(){
+    const res: any = await makeRequest('POST', `/lessons/update-progress`,null,{"lessonId": lesson.id,"progress": 199});
+    console.log('updated progress: ', res);
+    if(res.status !== 200) utils.createErrorNotification('Unable to update lesson progress', 1000);
+  }
   useEffect(() => {
-    const progress = !location.state.lesson?.progress ? 0 : location.state.lesson?.progress === topic.duration ? 0 : location.state.lesson?.progress;
+    if(!lesson || !hasLoadedProgress) return
+    const progress = !lesson.progress ? 0 : lesson.progress === topic.duration ? 0 : lesson?.progress;
     if (videoRef.current) {
       videoRef.current.currentTime = progress;
     }
-  }, [topic]);
+  }, [topic, lesson, hasLoadedProgress]);
 
   return (
     <div className="flex flex-col w-full gap-30">
